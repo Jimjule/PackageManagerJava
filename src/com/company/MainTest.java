6ebf9296f6b3834eb8fe9762f09ba099d329df8d
Jimjule <roadknightj@gmail.com>
2020-02-20 13:55:23 +0000

    First commit

---
+++ src/com/company/MainTest.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void testClassIsNotNull() {
+        Main main = new Main();
+        assertNotNull(main);
+    }
+}
