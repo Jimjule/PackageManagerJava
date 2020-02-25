7e4cb75b7bfd2ec640935670fd61e42b68b07088
Jimjule <roadknightj@gmail.com>
2020-02-25 09:10:14 +0000

    Removes validation for clean pr

--- src/com/company/Input.java
+++ src/com/company/Input.java
@@ -1,6 +1,5 @@
 package com.company;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Input {
 
@@ -8,12 +7,7 @@ public class Input {
     }
 
     public static String confirmInput(String detail) {
-        String input = getInput(detail);
-        Boolean validInput = validateInput(detail, input);
-        if (validInput == false) {
-            confirmInput(detail);
-        }
-        return input;
+        return getInput(detail);
     }
 
     public static Integer menuChoice() {
@@ -31,40 +25,5 @@ public class Input {
         System.out.println("Please enter your " + detail + ":");
         String input = userInput.nextLine();
         return input;
-    }
-
-    public static Boolean validateInput(String detail, String input) {
-        switch (detail) {
-            case "first name":
-            case "last name":
-                return validName(input);
-            case "phone number": return validNumber(input);
-            case "DOB in dd/mm/yy format": return validDOB(input);
-            case "email": return validEmail(input);
-            default: return true;
-        }
-    }
-
-    public static Boolean validName(String name) {
-        Pattern namePattern = Pattern.compile("^[A-Z]'?[- a-zA-Z]+$");
-        return name.matches(String.valueOf(namePattern));
-    }
-    public static Boolean validNumber(String phoneNumber) {
-        Pattern phonePattern = Pattern.compile("^[\\d]+$");
-        return phoneNumber.matches(String.valueOf(phonePattern));
-    }
-
-    public static Boolean validDOB(String dOB) {
-        Pattern dOBPattern = Pattern.compile("(\\d{2}\\/\\d{2}\\/\\d{4})");
-        if (dOB.matches(String.valueOf(dOBPattern))) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public static Boolean validEmail(String email) {
-        Pattern emailPattern = Pattern.compile("^(.+@.+)$");
-        return email.matches(String.valueOf(emailPattern));
     }
 }
--- src/com/company/InputTest.java
+++ src/com/company/InputTest.java
@@ -1,10 +1,7 @@
 package com.company;
-
 import org.junit.Test;
-
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-
 import static org.junit.Assert.*;
 
 public class InputTest {
@@ -31,63 +28,5 @@ public class InputTest {
         InputStream in = new ByteArrayInputStream(testInput.getBytes());
         System.setIn(in);
         assertNotNull(input.menuChoice());
-    }
-
-    @Test
-    public void testValidatesInput() {
-        Input input = new Input();
-        String testInput = "077075643";
-        InputStream in = new ByteArrayInputStream(testInput.getBytes());
-        System.setIn(in);
-        assertTrue(input.validNumber(input.getInput("phone number")));
-    }
-
-    @Test
-    public void testInvalidInput() {
-        Input input = new Input();
-        assertFalse(input.validNumber("07d7075643"));
-    }
-
-    @Test
-    public void testValidInput() {
-        Input input = new Input();
-        assertTrue(input.validateInput("phone number","077075643"));
-    }
-
-    @Test
-    public void testValidateNumber() {
-        Input input = new Input();
-        assertTrue(input.validNumber("077075643"));
-    }
-
-
-    @Test
-    public void testFailValidateNumber() {
-        Input input = new Input();
-        assertFalse(input.validNumber("077D75643"));
-    }
-
-    @Test
-    public void testValidateDOB() {
-        Input input = new Input();
-        assertTrue(input.validDOB("10/02/1999"));
-    }
-
-    @Test
-    public void testFailValidateDOB() {
-        Input input = new Input();
-        assertFalse(input.validDOB("10-02-99"));
-    }
-
-    @Test
-    public void testValidateEmail() {
-        Input input = new Input();
-        assertTrue(input.validEmail("this@that"));
-    }
-
-    @Test
-    public void testFailValidateEmail() {
-        Input input = new Input();
-        assertFalse(input.validEmail("thisatthat"));
     }
 }
